92389add28f1fff1d23f87193e7d4c2fb7fd9bbe
Luis Ron <116908552+LuisRon27@users.noreply.github.com>
2023-02-26 20:09:53 -0300

    maqueta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { weatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  cityName = 'wellington';
+  lastSearchedCity: string = '';
+  weatherData: weatherData | undefined;
+
+  async ngOnInit() {
+    await this.getWeatherData(this.cityName);
+  }
+
+  clearInput() {
+    this.cityName = '';
+  }
+  onSubmit() {
+    this.lastSearchedCity = this.cityName; // guarda el nombre de la ciudad
+    this.getWeatherData(this.cityName);
+    this.clearInput();
+  }
+
+  
+
+  private async getWeatherData(cityName: string) {
+    const response = await this.weatherService.getWeatherData(cityName).toPromise();
+    this.weatherData = response;
+    console.log(response);
+  }
 }
